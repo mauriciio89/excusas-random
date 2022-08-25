72addbd715d1096a96935dfda0daeeb506064310
mauriciio89 <maugalvez89@gmail.com>
2022-08-25 02:49:23 +0000

    cambios

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const who = ['the dog','my granma','his turtle','my bird'];
-const what = ['eat','pissed','crushed','broked'];
-const when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+const who = ["mi perro", "mi abuela", "mi jirafa", "mi dinosaurio"];
+const what = ["murio", "se cago encima", "se rompio el cuello", "se extinguió"];
+const when = [
+  "cuando iba al baño",
+  "cuando rezaba",
+  "cuando corría",
+  "cuando estaba almorzando",
+  "cuando perseguía a mi oruga",
+];
 
 const getRandomInt = (max) => {
-  const x = Math.floor(Math.random() * (max - 1))
+  const x = Math.floor(Math.random() * (max - 1));
   return x;
-}
+};
 
-window.addEventListener('DOMContentLoaded', () => {
-  const excuse = who[getRandomInt(who.length)] + ' ' + what[getRandomInt(what.length)] + ' ' + when[getRandomInt(when.length)]
-  document.getElementById('excuse').innerText = excuse
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const excuse =
+    who[getRandomInt(who.length)] +
+    " " +
+    what[getRandomInt(what.length)] +
+    " " +
+    when[getRandomInt(when.length)];
+  document.getElementById("excuse").innerText = excuse;
+});
